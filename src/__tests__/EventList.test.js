eff04567c807ab43a7c7d0af8dc5e91eefaa574a
Marline John <marlinejohn@gmail.com>
2024-04-08 22:42:36 +0200

    create EventList.test.js and Event component and import Event component in EventList.js

---
+++ src/__tests__/EventList.test.js
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+import EventList from '../components/EventList';
+
+describe('<EventList /> component', () => {
+  test('has an element with "list" role', () => {
+    const EventListComponent = render(<EventList />);
+    expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
+  });
+
+  test('renders correct number of events', () => {
+    const EventListComponent = render(<EventList events={[{}, {}, {}, {}]} />);
+    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
+  });
+});
