a170d6912a7e039e80fcad095096a8150c7c75f1
Marline John <marlinejohn@gmail.com>
2024-04-09 17:28:43 +0200

    add handleInputChanged function to CitySearch, add code in api.js

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -11,10 +11,10 @@ describe('<EventList /> component', () => {
     expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
   });
 
-  test('renders correct number of events', () => {
-    EventListComponent.rerender(<EventList events={
-        [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
-    } />);
+  test("renders correct number of events", () => {
+    EventListComponent.rerender(
+      <EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />
+    );
     expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
   });
 });
