65027ee62cb27e39b7fdf5d765e5c3142637d8b8
Marline John <marlinejohn@gmail.com>
2024-04-11 01:06:26 +0200

    write test to render 32 events as default value

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,13 +1,18 @@
 import { render } from '@testing-library/react';
+import NumberOfEvents from "../components/NumberOfEvents";
 
 describe('<NumberOfEvents /> component' , ()=> {
     let NumberOfEventsComponent;
     beforeEach(() => {
-        NumberOfEventsComponent = render(<NumberOfEvents  />);
+        NumberOfEventsComponent = render(<NumberOfEvents eventsSetValue={() => { }} />);
       });
 
       test('has element with role "textbox"', ()=>{
         let textbox = NumberOfEventsComponent.queryByRole('textbox');
         expect(textbox).toBeInTheDocument();
       })
+
+      test('render 32 events as default', () => {
+        expect(NumberOfEventsComponent.queryByRole('textbox')).toHaveValue('32');
+      });
 })
--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,7 +1,21 @@
-const NumberOfEvents = () => {
+import { useState } from "react";
+
+const NumberOfEvents = ({eventsSetValue}) => {
+    const[numOfEvents, setNumOfEvents] = useState('32')
+
+    const handleInputChanged = (event)=>{
+        const value = event.target.value;
+        setNumOfEvents(value)
+        eventsSetValue(value)
+    }
+
     return (
         <div id="no-of-events">
-            <input />
+            <input 
+            type= "text"
+            value={numOfEvents}
+            onChange={handleInputChanged}
+            />
         </div>
     )
 }
