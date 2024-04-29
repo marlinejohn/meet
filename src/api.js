95089884cdaa44893b0ad399f04b1697bbb57777
Marline John <marlinejohn@gmail.com>
2024-04-29 20:44:11 +0200

    implement offline capability

--- src/api.js
+++ src/api.js
@@ -30,6 +30,11 @@ export const getEvents = async () => {
   if (window.location.href.startsWith('http://localhost')) {
     return mockData;
   }
+
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    return events?JSON.parse(events):[];
+  }
   const token = await getAccessToken();
 
   if (token) {
@@ -38,6 +43,7 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null; 
   }
