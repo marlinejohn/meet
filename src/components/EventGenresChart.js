5440e0e5684307294427f9493be8b67e6db182ff
Marline John <marlinejohn@gmail.com>
2024-05-03 01:17:49 +0200

    update piechart

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -49,7 +49,7 @@ const EventGenresChart = ({events}) =>{
               fill="#8884d8"
               labelLine={false}
               label = {renderCustomizedLabel}
-              outerRadius={150}           
+              outerRadius={130}           
             />
           </PieChart>
         </ResponsiveContainer>
